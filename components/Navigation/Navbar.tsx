4c449cfbb458f094ef20dc28c98981b8ddf8d621
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-06 15:31:38 -0500

    Test styles with StyledComponents

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components';
+
 export default function Navbar() {
+    
+    const Navbar = styled.nav`
+        background-color: black;
+        height: 1000px;
+        width: 500px;
+    `
+    
     return (
-        <nav className="navbar">
-            
-        </nav>
+        <Navbar>
+
+        </Navbar>
     )
 }
