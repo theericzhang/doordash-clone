389fae3723165edde837cce253694057e676a034
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-06 16:37:45 -0500

    Declared global styles, link tags in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,70 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-Heavy.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-Black.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-ExtraBold.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-Bold.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-Medium.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-Light.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-ExtraLight.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/TTNorms/TTNorms-Thin.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin=""
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
---
+++ styles/GlobalStyles.tsx
@@ -0,0 +1,67 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    :root {
+        /* System Colors */
+        --primary-gray: rgba(231, 231, 231, 1);
+        --primary-white: rgba(255, 255, 255, 1);
+        --primary-red: rgba(255, 48, 8, 1);
+    }
+
+    /* System Fonts */
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 800;
+        src: url("/fonts/TTNorms/TTNorms-Heavy.woff2") format("woff2");
+    }
+    
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 700;
+        src: url("/fonts/TTNorms/TTNorms-Black.woff2") format("woff2");
+    }
+    
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 600;
+        src: url("/fonts/TTNorms/TTNorms-ExtraBold.woff2") format("woff2");
+    }
+    
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 500;
+        src: url("/fonts/TTNorms/TTNorms-Bold.woff2") format("woff2");
+    }
+    
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 400;
+        src: url("/fonts/TTNorms/TTNorms-Medium.woff2") format("woff2");
+    }
+    
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 300;
+        src: url("/fonts/TTNorms/TTNorms-Light.woff2") format("woff2");
+    }
+    
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 200;
+        src: url("/fonts/TTNorms/TTNorms-ExtraLight.woff2") format("woff2");
+    }
+    
+    @font-face {
+        font-family: TTNorms;
+        font-style: normal;
+        font-weight: 100;
+        src: url("/fonts/TTNorms/TTNorms-Thin.woff2") format("woff2");
+    }
+`
