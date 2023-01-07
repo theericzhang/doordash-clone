0a6a2f43d57f88ed7b699997b1b4869e38b7f571
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-07 02:31:38 -0500

    Note that if CSS styles are dependent on TS, you need to invoke the styled function this way - announcing the prop ahead, at style declaration

--- components/Navigation/SearchBar.tsx
+++ components/Navigation/SearchBar.tsx
@@ -6,7 +6,9 @@ type TSearchBar = {
     setIsSearchBarToggled: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const SearchBar__wrapper = styled.div`
+// Note that if CSS styles are dependent on TS, you need to invoke the styled function this way - announcing the prop ahead, at style declaration
+
+const SearchBar__wrapper = styled("div")<{isSearchBarToggled: boolean}>`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -15,6 +17,8 @@ const SearchBar__wrapper = styled.div`
     padding: 0 13px;
     width: fit-content;
     border-radius: 23px;
+    border: 2px solid var(--primary-black);
+    border: ${props => props.isSearchBarToggled ? "2px solid var(--primary-black)" : "none"};
     background-color: var(--secondary-gray);
 `;
 
@@ -53,6 +57,7 @@ export default function SearchBar({ isSearchBarToggled, setIsSearchBarToggled }:
         <SearchBar__wrapper 
             onClick={searchBarOnClick}
             onBlur={searchBarOnBlur}
+            isSearchBarToggled={isSearchBarToggled}
         >
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path fillRule="evenodd" clipRule="evenodd" d="M14.1922 15.6064C13.0236 16.4816 11.5723 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 11.5723 16.4816 13.0236 15.6064 14.1922L20.7071 19.2929C21.0976 19.6834 21.0976 20.3166 20.7071 20.7071C20.3166 21.0976 19.6834 21.0976 19.2929 20.7071L14.1922 15.6064ZM15 10C15 12.7614 12.7614 15 10 15C7.23858 15 5 12.7614 5 10C5 7.23858 7.23858 5 10 5C12.7614 5 15 7.23858 15 10Z" fill="#494949"></path></svg>
             <SearchBar__input
