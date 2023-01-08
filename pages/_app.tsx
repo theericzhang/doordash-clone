c0f5ed00d289680933b65b05a96e23d8add4207a
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-08 15:27:02 -0500

    Enable SSR style injection for styled components under SWC. Fixed FOUT with custom _document.tsx page. SEE DESCRIPTION
    https://dev.to/dharmi/adding-fonts-in-nextjs-local-fonts-along-with-styled-components-2cdd
    
    tl;dr, you need the ServerStyleSheet class from styled-components to enable SSR styles injection - otherwise a flash of unstyled content will appear on first page loads.
    
    With Babel/SWC, you can check this. Consider using SWC as it's Next.js default. - https://styled-components.com/docs/advanced#server-side-rendering
    
    For FOUT, you need to manually create a Head tag to preload all fonts that are used in the app. These can be declared in the same _document.tsx file, underneath the render() function.
    
    Optimizing fonts also requires you to let Next know that these fonts exist. Check _app.tsx for details on this optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,62 @@
 import "../styles/globals.css";
 import { GlobalStyles } from "../styles/GlobalStyles";
+import localFont from '@next/font/local';
 import type { AppProps } from "next/app";
 import HomeLayout from "../components/Layouts/HomeLayout";
 
+const ttNormsFont = localFont({
+    src: [
+        {
+            path:'../public/fonts/TTNorms/TTNorms-Heavy.woff2',
+            weight: '800',
+            style: 'normal',
+        },
+        {
+            path:'../public/fonts/TTNorms/TTNorms-Black.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path:'../public/fonts/TTNorms/TTNorms-ExtraBold.woff2',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path:'../public/fonts/TTNorms/TTNorms-Bold.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path:'../public/fonts/TTNorms/TTNorms-Medium.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path:'../public/fonts/TTNorms/TTNorms-Light.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path:'../public/fonts/TTNorms/TTNorms-ExtraLight.woff2',
+            weight: '200',
+            style: 'normal',
+        },
+        {
+            path:'../public/fonts/TTNorms/TTNorms-Thin.woff2',
+            weight: '100',
+            style: 'normal',
+        }
+    ]
+})
+
 export default function App({ Component, pageProps }: AppProps) {
+    console.log('%c https://anericzhang.com', 'font-weight: bold; font-size: 50px; color: orange;');
     return (
-        <>
+        <div className={ttNormsFont.className}>
             <GlobalStyles />
             <HomeLayout>
                 <Component {...pageProps} />
             </HomeLayout>
-        </>
+        </div>
     );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,70 +1,95 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-Heavy.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-Black.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-ExtraBold.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-Bold.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-Medium.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-Light.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-ExtraLight.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/TTNorms/TTNorms-Thin.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin=""
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: [initialProps.styles, sheet.getStyleElement()],
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-Heavy.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-Black.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-ExtraBold.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-Bold.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-Medium.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-Light.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-ExtraLight.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/TTNorms/TTNorms-Thin.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin=""
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
