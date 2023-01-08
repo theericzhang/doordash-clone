29ddd991ade917b16699471cd9c984da9419377e
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-07 19:32:41 -0500

    remove styles declaration in HomeLayout

--- components/Layouts/HomeLayout.tsx
+++ components/Layouts/HomeLayout.tsx
@@ -1,5 +1,4 @@
 import Navbar from "../Navigation/Navbar";
-import styles from "../../styles/Home.module.css";
 import { GlobalStyles } from "../../styles/GlobalStyles";
 
 export default function HomeLayout({ children }: any) {
