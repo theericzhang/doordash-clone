f9d06f93f85724084034b8a53df24a9546c94ff5
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-07 20:44:46 -0500

    alignment of button rows

--- components/HomeFilters/FilterButtonRow.tsx
+++ components/HomeFilters/FilterButtonRow.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import FilterButton from "./FilterButton";
 
 const FilterButtonRow__wrapper = styled.div`
+    display: flex;
+    align-items: center;
     width: 100%;
     height: 63px;
     background-color: var(--primary-white);
