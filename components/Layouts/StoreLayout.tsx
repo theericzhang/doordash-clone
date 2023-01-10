db3d903dfefbb3ac855b1cd77f61ebeef4e0f384
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-09 20:49:34 -0500

    Wired [slug] to grab serversideprops

--- components/Layouts/StoreLayout.tsx
+++ components/Layouts/StoreLayout.tsx
@@ -1,7 +1,14 @@
 import HeroComponent from "../StoreComponents/HeroComponent/HeroComponent";
 
-export default function StoreLayout() {
+type TStoreLayout = {
+    storeID: string;
+}
+
+export default function StoreLayout({ storeID }: TStoreLayout) {
     return (
-        <HeroComponent />
+        <>
+            <HeroComponent />
+            {storeID}
+        </>
     );
 }
--- pages/store/[slug].tsx
+++ pages/store/[slug].tsx
@@ -1,7 +1,24 @@
 import StoreLayout from "../../components/Layouts/StoreLayout";
+import { GetServerSidePropsContext } from "next";
 
-export default function Store() {
+type TServerSideProps = {
+    storeID: string;
+}
+
+export default function Store({ storeID }: TServerSideProps) {
+    console.log(storeID);
     return (
-        <StoreLayout />
+        <StoreLayout 
+            storeID={storeID}
+        />
     );
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+    // fetch data pertinent to store here
+    // Always note that the last property ctx.params?.slug MUST be the same as the name [slug], without square brackets.
+    const storeID = ctx.params?.slug;
+    return {
+        props: { storeID }
+    }
 }
