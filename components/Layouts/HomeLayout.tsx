44f4fd2c0b1fa32036b9af9965ffbf77c716c1ab
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-10 14:06:04 -0500

    restructuring index.tsx, Head not injected in HomeLayout

--- components/Layouts/HomeLayout.tsx
+++ components/Layouts/HomeLayout.tsx
@@ -1,6 +1,9 @@
+import FilterButtonRow from "../FilterButtonRow/FilterButtonRow"
+
 export default function HomeLayout({ children }: any) {
     return (
         <>
+            <FilterButtonRow />
             {children}
         </>
     )
