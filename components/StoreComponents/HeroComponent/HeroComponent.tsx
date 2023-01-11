b22439ed5c77ba500779d0edb5e8bf44f6ba6761
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-10 20:18:13 -0500

    Wrapper of HeroComponent set

--- components/StoreComponents/HeroComponent/HeroComponent.tsx
+++ components/StoreComponents/HeroComponent/HeroComponent.tsx
@@ -1,4 +1,32 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+
+const HeroComponent__wrapper = styled.section`
+    display: flex;
+    margin: 17px 0;
+    width: 100%;
+`;
+
+const HeroComponent__inner = styled.div`
+    display: flex;
+    width: 928px;
+    margin: 0 auto;
+`;
+
+const HeroComponent__image__wrapper = styled.div`
+    width: 100%;
+    height: 250px;
+    border-radius: 15px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+`;
+
+const HeroComponent__image = styled(Image)`
+    object-fit: cover;
+`;
 
 type TRestaurantData = {
     restaurantData: {
@@ -11,6 +39,18 @@ type TRestaurantData = {
 
 export default function HeroComponent({ restaurantData }: TRestaurantData) {
     return (
-        <h3>Hello! {restaurantData.restaurantName}</h3>
+        <HeroComponent__wrapper>
+            <HeroComponent__inner>
+                <HeroComponent__image__wrapper>
+                    <HeroComponent__image 
+                        src={restaurantData.restaurantImage}
+                        alt={`Image from ${restaurantData.restaurantName}`}
+                        placeholder="blur"
+                        blurDataURL="/images/Blur.png"
+                        fill={true}
+                    />
+                </HeroComponent__image__wrapper>
+            </HeroComponent__inner>
+        </HeroComponent__wrapper>
     );
 }
