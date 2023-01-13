173a5467661bb1a1d12167cf8cdfcf4ff797155e
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-13 17:30:29 -0500

    CheckoutButton labels and wrapper implemented

--- components/StoreComponents/CheckoutButton/CheckoutButton.tsx
+++ components/StoreComponents/CheckoutButton/CheckoutButton.tsx
@@ -13,10 +13,32 @@ const CheckoutButton__button = styled.button`
     }
 `;
 
+const CheckoutButton__text__wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+    width: 100%;
+`;
+
+const CheckoutButton__checkout__label = styled.span`
+    font-size: 16px;
+    font-weight: 500;
+    color: var(--primary-white);
+`;
+
+const CheckoutButton__price__label = styled.span`
+    font-size: 16px;
+    font-weight: 500;
+    color: var(--primary-white);
+`;
+
 export default function CheckoutButton() {
     return (
         <CheckoutButton__button>
-            
+            <CheckoutButton__text__wrapper>
+                <CheckoutButton__checkout__label>Checkout</CheckoutButton__checkout__label>
+                <CheckoutButton__price__label>$55.65</CheckoutButton__price__label>
+            </CheckoutButton__text__wrapper>
         </CheckoutButton__button>
     );
 }
