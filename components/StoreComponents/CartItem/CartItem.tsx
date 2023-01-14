82ba4061ab50d5f5d967ff0e9314ed24b971c201
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-13 22:17:23 -0500

    Finetune text overflow behavior for cart menu items

--- components/StoreComponents/CartItem/CartItem.tsx
+++ components/StoreComponents/CartItem/CartItem.tsx
@@ -9,13 +9,14 @@ const CartItem__wrapper = styled.a`
     align-items: center;
     padding: 16px 0;
     padding-left: 5px;
+    padding-right: 16px;
     border-bottom: 1px solid var(--primary-gray);
 `;
 
 const CartItem__image__wrapper = styled.div`
     width: 60px;
     height: 60px;
-    border-radius: 3px;
+    border-radius: 5px;
     overflow: hidden;
     display: flex;
     justify-content: center;
@@ -30,11 +31,24 @@ const CartItem__image = styled(Image)`
 const CartItem__description__wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    row-gap: 10px;
+    row-gap: 12px;
+    margin-right: 9.5px;
+    max-width: 98px;
 `;
 
-const CartItem__description__span = styled.span`
+const CartItem__description__item = styled.span`
+    max-width: 100%;
     font-size: 16px;
+    font-weight: 400;
+    color: var(--primary-black);
+    line-height: 22px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+`;
+
+const CartItem__description__price = styled.span`
+    font-size: 14px;
     font-weight: 400;
     color: var(--primary-black);
 `;
@@ -51,12 +65,12 @@ export default function CartItem() {
                 />
             </CartItem__image__wrapper>
             <CartItem__description__wrapper>
-                <CartItem__description__span>
-                    Garlic Knots
-                </CartItem__description__span>
-                <CartItem__description__span>
+                <CartItem__description__item>
+                    Rice & Black Beans Rice & Black Beans Rice & Black Beans Rice & Black Beans Rice & Black Beans
+                </CartItem__description__item>
+                <CartItem__description__price>
                     $8.99
-                </CartItem__description__span>
+                </CartItem__description__price>
             </CartItem__description__wrapper>
             <InputStepper />
         </CartItem__wrapper>
