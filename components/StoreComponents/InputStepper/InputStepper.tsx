a1da6bfb1c1d283eb4be38e5f63810a4901aac77
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-13 21:57:08 -0500

    Added additional finetuned styles and icons

--- components/StoreComponents/InputStepper/InputStepper.tsx
+++ components/StoreComponents/InputStepper/InputStepper.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import styled from "styled-components";
+import Minus from "../../Icons/MinusIcon";
+import Plus from "../../Icons/PlusIcon";
+import GarbageCan from "../../Icons/GarbageCanIcon";
 
 const InputStepper__wrapper = styled.div`
     display: flex;
     align-items: center;
+    background-color: var(--secondary-gray);
+    border-radius: 17px;
+    box-shadow: rgb(0 0 0 / 20%) 0px 2px 8px;
 `;
 
 const InputStepper__button = styled.button`
@@ -12,10 +18,20 @@ const InputStepper__button = styled.button`
     align-items: center;
     width: 32px;
     height: 32px;
+    border-radius: 500px;
+    background-color: var(--primary-white);
+`;
+
+const InputStepper__label__wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    min-width: 32px;
 `;
 
 const InputStepper__label = styled.span`
-    
+    font-weight: 500;
+    font-size: 14px;
+    color: var(--primary-black);
 `;
 
 export default function InputStepper() {
@@ -30,6 +46,8 @@ export default function InputStepper() {
     function handleClickDecrement() {
         if (stepperCount > 0) {
             setStepperCount(prevStepperCount => prevStepperCount - 1);
+            // delete item and remove from list if stepperCount reaches 0.
+            // think of passing an ID that react can remove from an array of cart items.
         } else {return}
     }
 
@@ -37,13 +55,23 @@ export default function InputStepper() {
         <InputStepper__wrapper>
             <InputStepper__button 
                 onClick={handleClickDecrement}
-            />
-            <InputStepper__label>
-                {stepperCount}
-            </InputStepper__label>
+            >
+                {stepperCount === 1 ?
+                    <GarbageCan />
+                :
+                    <Minus />
+                }
+            </InputStepper__button>
+            <InputStepper__label__wrapper>
+                <InputStepper__label>
+                    {stepperCount} ×
+                </InputStepper__label>
+            </InputStepper__label__wrapper>
             <InputStepper__button 
                 onClick={handleClickIncrement}
-            />
+            >
+                <Plus />
+            </InputStepper__button>
         </InputStepper__wrapper>
     );
 }
