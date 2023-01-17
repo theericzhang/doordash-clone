1ad911adda83b39736e8946c5d6d783f98a20071
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-17 14:29:00 -0500

    additional item text elements

--- components/StoreComponents/MenuSection/MenuSection.tsx
+++ components/StoreComponents/MenuSection/MenuSection.tsx
@@ -52,6 +52,7 @@ const MenuSection__item = styled.button`
     border-radius: 4px;
     transition: 0.25s ease;
     transition-property: border-color;
+    align-items: center;
 
     &:hover {
         cursor: pointer;
@@ -59,7 +60,39 @@ const MenuSection__item = styled.button`
         transition: 0.25s ease;
         transition-property: border-color;
     }
-`
+`;
+
+const Item__text__wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0 16px;
+    row-gap: 5px;
+`;
+
+const Item__text__name = styled.span`
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: -0.4px;
+    color: var(--primary-black);
+`;
+
+const Item__text__description = styled.span`
+    font-size: 14px;
+    font-weight: 400;
+    color: var(--quinary-gray);
+`;
+
+const Item__text__stats__wrapper = styled.div`
+    display: flex;
+`;
+
+const Item__text__stats = styled.span`
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: -0.3px;
+    color: var(--secondary-black);
+`;
 
 export default function MenuSection() {
     return (
@@ -73,6 +106,22 @@ export default function MenuSection() {
                 </MenuSection__subheader>
             </MenuSection__header__wrapper>
             <MenuSection__items__wrapper>
+                <MenuSection__item>
+                    <Item__text__wrapper>
+                        <Item__text__name>
+                            12 piece Omakase
+                        </Item__text__name>
+                        <Item__text__description>
+                            Chef&apos;s choice of daily sushi
+                        </Item__text__description>
+                        <Item__text__stats__wrapper>
+                            <Item__text__stats>
+                                $8.95
+                            </Item__text__stats>
+                        </Item__text__stats__wrapper>
+                    </Item__text__wrapper>
+                </MenuSection__item>
+                {/* <MenuSection__item />
                 <MenuSection__item />
                 <MenuSection__item />
                 <MenuSection__item />
@@ -80,8 +129,7 @@ export default function MenuSection() {
                 <MenuSection__item />
                 <MenuSection__item />
                 <MenuSection__item />
-                <MenuSection__item />
-                <MenuSection__item />
+                <MenuSection__item /> */}
 
             </MenuSection__items__wrapper>
         </MenuSection__section>
