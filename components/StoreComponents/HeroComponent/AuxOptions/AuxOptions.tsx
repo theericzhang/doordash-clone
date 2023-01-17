9bb3f682ff7215a4186ad77ec03c7e6bd783cc26
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-16 19:28:24 -0500

    wrapper now acts as a button to toggle between isDelivery states

--- components/StoreComponents/HeroComponent/AuxOptions/AuxOptions.tsx
+++ components/StoreComponents/HeroComponent/AuxOptions/AuxOptions.tsx
@@ -36,48 +36,86 @@ const AuxOptions__Toggle__wrapper = styled.div`
     border-radius: 20px;
     position: relative;
     overflow: hidden;
-
-    &:hover {
-        cursor: pointer;
-    }
-`;
-
-const AuxOptions__Toggle = styled.button<{ isDelivery: boolean }>`
-    display: flex;
-    justify-content: center;
-    height: 100%;
-    background-color: ${props => props.isDelivery ? 'var(--primary-black)' : 'var(--primary-gray)'};
-    align-items: center;
-    border: none;
-    border-radius: ${props => props.isDelivery ? '20px' : '0'};
-    padding: 0 24px;
+    width: 195px;
     transition: 0.15s ease;
     transition-property: background-color;
+    justify-content: space-around;
 
     &:hover {
         cursor: pointer;
         transition: 0.15s ease;
         transition-property: background-color;
-        background-color: ${props => props.isDelivery ? 'var(--tertiary-gray)' : 'var(--secondary-gray)'};
+        background-color: var(--secondary-gray);
+    }
+`;
+
+const AuxOptions__Toggle__slider = styled.div<{isDelivery: boolean}>`
+    display: flex;
+    background-color: var(--primary-black);
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding: 0 24px;
+    border-radius: 20px;
+    transition: 0.15s ease;
+    transition-property: background-color;
+    position: absolute;
+    left: ${props => props.isDelivery ? '0px' : 'unset'}; // something's wrong here
+    right: ${props => !props.isDelivery ? '0px' : 'unset'};
+    z-index: 1;
+
+    &:hover {
+        cursor: pointer;
+        transition: 0.15s ease;
+        transition-property: background-color;
+        background-color: var(--tertiary-gray);
     }
 `;
 
 const AuxOptions__Toggle__label__wrapper = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     row-gap: 1px;
     margin-bottom: 1px;
 `;
 
-const AuxOptions__Toggle__label__small = styled.span<{isDelivery: boolean}>`
+const AuxOptions__Toggle__label__wrapper__stationary__left = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 1px;
+    margin-bottom: 1px;
+`
+
+const AuxOptions__Toggle__label__wrapper__stationary__right = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 1px;
+    margin-bottom: 1px;
+`
+
+const AuxOptions__Toggle__label__small = styled.span`
     font-size: 12px;
-    color: ${props => props.isDelivery ? `var(--quaternary-gray)` : `var(--secondary-black)`};
+    color: var(--quaternary-gray);
 `;
 
-const AuxOptions__Toggle__label = styled.span<{isDelivery: boolean}>`
+const AuxOptions__Toggle__label = styled.span`
     font-size: 14px;
     font-weight: 500;
-    color: ${props => props.isDelivery ? `var(--primary-white)` : `var(--primary-black)`};
+    color: var(--primary-white);
+`;
+
+const AuxOptions__Toggle__label__small__stationary = styled.span`
+    font-size: 12px;
+    color: var(--secondary-black);
+`;
+
+const AuxOptions__Toggle__label__stationary = styled.span`
+    font-size: 14px;
+    font-weight: 500;
+    color: var(--primary-black);
 `;
 
 export default function AuxOptions() {
@@ -96,33 +134,40 @@ export default function AuxOptions() {
                     Group Order
                 </AuxOptions__Button__label>
             </AuxOptions__Button__primary>
-            <AuxOptions__Toggle__wrapper>
-                <AuxOptions__Toggle 
+            {/* TODO: STOP EVENT PROPOGATION TO SLIDER COMPONENT. ANIMATE SLIDER COMPONENT TO MOVE ACROSS. CLEAN UP UNNEEDED CSS */}
+            <AuxOptions__Toggle__wrapper onClick={(e) => {
+                setIsDelivery(prevIsDelivery => !prevIsDelivery);
+            }}>
+                <AuxOptions__Toggle__slider 
                     isDelivery={isDelivery}
-                    disabled={isDelivery}
-                    onClick={() => {setIsDelivery(prevIsDelivery => !prevIsDelivery)}}>
+                    onClick={(e) => e.stopPropagation()}
+                >
                     <AuxOptions__Toggle__label__wrapper>
-                        <AuxOptions__Toggle__label isDelivery={isDelivery}>
-                            Delivery
+                        <AuxOptions__Toggle__label>
+                            {isDelivery ? 'Delivery' : 'Pickup'}
                         </AuxOptions__Toggle__label>
-                        <AuxOptions__Toggle__label__small isDelivery={isDelivery}>
-                            22 min
+                        <AuxOptions__Toggle__label__small>
+                            {isDelivery ? '22' : '14'} min
                         </AuxOptions__Toggle__label__small>
                     </AuxOptions__Toggle__label__wrapper>
-                </AuxOptions__Toggle>
-                <AuxOptions__Toggle 
-                    isDelivery={!isDelivery}
-                    disabled={!isDelivery}
-                    onClick={() => {setIsDelivery(prevIsDelivery => !prevIsDelivery)}}>
-                    <AuxOptions__Toggle__label__wrapper>
-                        <AuxOptions__Toggle__label isDelivery={!isDelivery}>
-                            Pickup
-                        </AuxOptions__Toggle__label>
-                        <AuxOptions__Toggle__label__small isDelivery={!isDelivery}>
-                            14 min
-                        </AuxOptions__Toggle__label__small>
-                    </AuxOptions__Toggle__label__wrapper>
-                </AuxOptions__Toggle>
+                </AuxOptions__Toggle__slider>
+
+                <AuxOptions__Toggle__label__wrapper__stationary__left>
+                    <AuxOptions__Toggle__label__stationary>
+                        Delivery
+                    </AuxOptions__Toggle__label__stationary>
+                    <AuxOptions__Toggle__label__small__stationary>
+                        22 min
+                    </AuxOptions__Toggle__label__small__stationary>
+                </AuxOptions__Toggle__label__wrapper__stationary__left>
+                <AuxOptions__Toggle__label__wrapper__stationary__right>
+                    <AuxOptions__Toggle__label__stationary>
+                        Pickup
+                    </AuxOptions__Toggle__label__stationary>
+                    <AuxOptions__Toggle__label__small__stationary>
+                        14 min
+                    </AuxOptions__Toggle__label__small__stationary>
+                </AuxOptions__Toggle__label__wrapper__stationary__right>
             </AuxOptions__Toggle__wrapper>
         </AuxOptions__wrapper>
     );
