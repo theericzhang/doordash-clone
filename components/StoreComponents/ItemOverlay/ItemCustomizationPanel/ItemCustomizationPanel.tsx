8322ef5855fb3e8e4276bfbc2295446f15ae6942
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-18 17:23:36 -0500

    images, ratings, description added

--- components/StoreComponents/ItemOverlay/ItemCustomizationPanel/ItemCustomizationPanel.tsx
+++ components/StoreComponents/ItemOverlay/ItemCustomizationPanel/ItemCustomizationPanel.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { useAppDispatch, useAppSelector } from "../../../../app-redux/hooks";
 import { toggleIsModalOpen } from "../../../../app-redux/features/item/itemSlice";
 import X from "../../../Icons/XIcon";
+import ThumbsUp from "../../../Icons/ThumbsUpIcon";
+import Image from "next/image";
 
 const ItemCustomizationPanel__wrapper = styled.div`
     display: flex;
@@ -28,6 +30,14 @@ const ItemCustomizationPanel__button_close = styled.button`
     }
 `;
 
+const ItemCustomizationPanel__content__wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 10px;
+    align-items: flex-start;
+    margin: 32px 0; 
+`;
+
 const ItemCustomizationPanel__item__name = styled.h2`
     font-size: 32px;
     font-weight: 500;
@@ -35,11 +45,39 @@ const ItemCustomizationPanel__item__name = styled.h2`
     letter-spacing: -0.45px;
 `;
 
-const ItemCustomizationPanl__item__description = styled.span`
+const ItemCustomizationPanel__stats__wrapper = styled.div`
+    display: flex;
+    column-gap: 4px;
+    align-items: center;
+`;
+
+const ItemCustomizationPanel__stats = styled.span`
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: -0.3px;
+    color: var(--secondary-black);
+`;
+
+const ItemCustomizationPanel__item__description = styled.span`
     font-size: 14px;
     font-weight: 400;
     color: var(--secondary-black);
-`
+`;
+
+const ItemCustomizationPanel__image__wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 295px;
+    overflow: hidden;
+    position: relative;
+`;
+
+const ItemCustomizationPanel__image = styled(Image)`
+    width: 100%;
+    object-fit: cover;
+`;
 
 export default function ItemCustomizationPanel() {
     const dispatch = useAppDispatch();
@@ -50,12 +88,41 @@ export default function ItemCustomizationPanel() {
             <ItemCustomizationPanel__button_close onClick={() => dispatch(toggleIsModalOpen())}>
                 <X />
             </ItemCustomizationPanel__button_close>
-            <ItemCustomizationPanel__item__name>
-                {itemData?.itemName}
-            </ItemCustomizationPanel__item__name>
-            <ItemCustomizationPanl__item__description>
-                {itemData?.description}
-            </ItemCustomizationPanl__item__description>
+            <ItemCustomizationPanel__content__wrapper>
+                <ItemCustomizationPanel__item__name>
+                    {itemData?.itemName}
+                </ItemCustomizationPanel__item__name>
+                {itemData?.ratingCount ? 
+                    <ItemCustomizationPanel__stats__wrapper>
+                        <ThumbsUp />
+                        <ItemCustomizationPanel__stats>
+                            {itemData.ratingPercentage}% ({itemData.ratingCount})
+                        </ItemCustomizationPanel__stats>
+                    </ItemCustomizationPanel__stats__wrapper>
+                :
+                    null
+                }
+                {itemData?.description ?
+                    <ItemCustomizationPanel__item__description>
+                        {itemData?.description}
+                    </ItemCustomizationPanel__item__description>
+                :
+                    null
+                }
+                {itemData?.image.src ? 
+                    <ItemCustomizationPanel__image__wrapper>
+                        <ItemCustomizationPanel__image 
+                            src={itemData.image.src}
+                            alt={itemData.image.alt}
+                            sizes={"295px"}
+                            fill={true}
+                        />
+                    </ItemCustomizationPanel__image__wrapper>
+                :
+                    null
+                }
+            </ItemCustomizationPanel__content__wrapper>
+
         </ItemCustomizationPanel__wrapper>
     );
 }
