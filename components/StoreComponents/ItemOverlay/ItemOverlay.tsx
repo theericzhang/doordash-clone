5a26ad1f997df48338dcc8d82aa9c9a25b61b56d
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-21 15:21:20 -0500

    using react-transition-group to define transition states with isModalOpen
    currently it only works with transition-out. Figure out how to make it work while transitioning in

--- components/StoreComponents/ItemOverlay/ItemOverlay.tsx
+++ components/StoreComponents/ItemOverlay/ItemOverlay.tsx
@@ -3,7 +3,10 @@ import { useAppSelector, useAppDispatch } from "../../../app-redux/hooks";
 import { toggleIsModalOpen } from "../../../app-redux/features/item/itemSlice";
 import ItemCustomizationPanel from "./ItemCustomizationPanel/ItemCustomizationPanel";
 
-const ItemModal__wrapper = styled.section`
+import { Transition } from "react-transition-group";
+import { useRef } from "react";
+
+const ItemModal__wrapper = styled.section<{ isModalOpen: boolean }>`
     width: 100%;
     height: 100%;
     position: fixed;
@@ -14,17 +17,41 @@ const ItemModal__wrapper = styled.section`
     display: flex;
     justify-content: center;
     align-items: center;
+    opacity: ${(props) => (props.isModalOpen ? 1 : 0)};
+    transition: opacity 300ms ease;
 `;
 
 export default function ItemModal() {
     const isModalOpen = useAppSelector((state) => state.itemSlice.isModalOpen);
     const dispatch = useAppDispatch();
+    const nodeRef = useRef(null);
     return (
-        isModalOpen ? 
-            <ItemModal__wrapper onClick={() => dispatch(toggleIsModalOpen())}>
-                <ItemCustomizationPanel />
-            </ItemModal__wrapper>
-        :
-            null
+        <Transition
+            nodeRef={nodeRef}
+            in={isModalOpen}
+            timeout={300}
+            unmountOnExit
+        >
+            {(state) => (
+                <ItemModal__wrapper 
+                    
+                    isModalOpen={isModalOpen}
+                    onClick={() => dispatch(toggleIsModalOpen())}
+                    ref={nodeRef}
+                >
+                    <ItemCustomizationPanel />
+                </ItemModal__wrapper>
+            )}
+        </Transition>
+
+        // isModalOpen ?
+        //     <ItemModal__wrapper
+        //         onClick={() => dispatch(toggleIsModalOpen())}
+        //         isModalOpen={isModalOpen}
+        //     >
+        //         <ItemCustomizationPanel />
+        //     </ItemModal__wrapper>
+        // :
+        //     null
     );
 }
