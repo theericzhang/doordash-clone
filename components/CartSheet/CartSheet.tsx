a4a004f32fb2e58fdb26bfd9c184cde2e1faa9c9
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-24 15:02:01 -0500

    Using global store to conditionally render the CartSheet__wrapper. Now we need to allow for the button to be pressed only when the button is on a page other than [slug]

--- components/CartSheet/CartSheet.tsx
+++ components/CartSheet/CartSheet.tsx
@@ -1,21 +1,26 @@
 import styled from "styled-components";
 import CartOverview from "../StoreComponents/CartOverviewComponent/CartOverview";
+import { useAppSelector, useAppDispatch } from "../../app-redux/hooks";
 import { toggleIsOpenFromCartSheet } from "../../app-redux/features/cart/cartSlice";
 
-const CartSheet__wrapper = styled.aside`
+const CartSheet__wrapper = styled.aside<{ isOpenFromCartSheet: boolean }>`
     display: flex;
     position: absolute;
     // width of the wrapper + box shadow = 340px + 24px
-    right: -364px;
+    right: ${props => props.isOpenFromCartSheet ? `0px` : `-364px`};
+    pointer-events: ${props => props.isOpenFromCartSheet ? `all` : `none`};
     z-index: 3;
     background-color: var(--primary-white);
     height: 100%;
     width: 340px;
+    transition: 0.225s right ease;
 `
 
 export default function CartSheet() {
+    const isOpenFromCartSheet = useAppSelector((state) => state.cartSlice.isOpenFromCartSheet);
+
     return (
-        <CartSheet__wrapper>
+        <CartSheet__wrapper isOpenFromCartSheet={!!isOpenFromCartSheet}>
             {/* Create an X button that triggers toggleIsOpenFromCartSheet */}
             <CartOverview isInCartSheet={true} />
         </CartSheet__wrapper>
