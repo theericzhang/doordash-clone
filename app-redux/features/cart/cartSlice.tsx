f4faca3b6c28a894bd3071675a4117e2a4d75092
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-01-26 11:23:50 -0500

    begin strict linting

--- app-redux/features/cart/cartSlice.tsx
+++ app-redux/features/cart/cartSlice.tsx
@@ -1,136 +1,136 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { restaurantList } from "../../../components/datav2";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { restaurantList } from '../../../components/datav2';
 
 interface ICartState {
-    storeID?: number;
-    isOpenFromCartSheet?: boolean;
-    pageViewingStoreID?: number;
-    cart: ICartItem[];
-    totalValue: number;
+  storeID?: number;
+  isOpenFromCartSheet?: boolean;
+  pageViewingStoreID?: number;
+  cart: ICartItem[];
+  totalValue: number;
 }
 
 interface ICartItem {
-    itemID: number;
-    quantity: number;
+  itemID: number;
+  quantity: number;
 }
 
 const initialState: ICartState = {
-    // storeID: 1243431,
-    isOpenFromCartSheet: false,
-    cart: [
-        // {
-        //     itemID: 0,
-        //     quantity: 2,
-        // },
-        // {
-        //     itemID: 1,
-        //     quantity: 2,
-        // },
-    ],
-    totalValue: 0,
+  // storeID: 1243431,
+  isOpenFromCartSheet: false,
+  cart: [
+    // {
+    //     itemID: 0,
+    //     quantity: 2,
+    // },
+    // {
+    //     itemID: 1,
+    //     quantity: 2,
+    // },
+  ],
+  totalValue: 0,
 };
 
 const restaurants = restaurantList;
 
 function calculateCartTotal() {
-    let sum = 0;
-    initialState.cart.forEach((item) => {
-        sum +=
-            item.quantity *
-            restaurants[initialState.storeID as keyof typeof restaurants]
-                .storefrontData.items[item.itemID].price;
-    });
-    console.log(initialState.totalValue);
-    return sum;
+  let sum = 0;
+  initialState.cart.forEach((item) => {
+    sum
+            += item.quantity
+            * restaurants[initialState.storeID as keyof typeof restaurants]
+              .storefrontData.items[item.itemID].price;
+  });
+  console.log(initialState.totalValue);
+  return sum;
 }
 
 initialState.totalValue = calculateCartTotal();
 
 function immutableCalculateCartTotal(state: ICartState) {
-    let sum = 0;
-    state.cart.forEach((item) => {
-        sum +=
-            item.quantity *
-            restaurants[state.storeID as keyof typeof restaurants]
-                .storefrontData.items[item.itemID].price;
-    });
-    state.totalValue = sum;
-    return;
+  let sum = 0;
+  state.cart.forEach((item) => {
+    sum
+            += item.quantity
+            * restaurants[state.storeID as keyof typeof restaurants]
+              .storefrontData.items[item.itemID].price;
+  });
+  state.totalValue = sum;
 }
 
 // cartSlice needs access to the storeID to ensure that all items in the cart are from storeID.
 // Without storeID validation, the cart could contain items from multiple stores, which is disallowed in standard DD delivery (with exception of having dasher pick up items after a delivery has been dispatched)
 const cartSlice = createSlice({
-    name: "cart",
-    initialState: initialState,
-    reducers: {
-        toggleIsOpenFromCartSheet: (state) => {
-            state.isOpenFromCartSheet = !state.isOpenFromCartSheet;
-        },
-
-        setPageViewingStoreID: (state, action: PayloadAction<number>) => {
-            state.pageViewingStoreID = action.payload;
-        },
-
-        setStoreID: (state, action: PayloadAction<number>) => {
-            state.storeID = action.payload;
-        },
-
-        // if the user adds an item from a different store, we need to reset the storeID and cart to the storeID of the new item they were looking at
-        resetCartNewStore: (state, action: PayloadAction<number>) => {
-            state.storeID = action.payload;
-            state.cart = [];
-            immutableCalculateCartTotal(state);
-        },
-
-        // the user adds an item, passing the itemID
-        addItemToCart: (state, action: PayloadAction<ICartItem>) => {
-            // loop through cart items to see if the itemID (action.payload) exists.
-            // if it exists, then just increment the quantity.
-            // if it does not exist, then add the new item to a copy of the existing cart state.
-
-            let itemExists = false;
-
-            for (let i = 0; i < state.cart.length; i++) {
-                if (state.cart[i].itemID === action.payload.itemID) {
-                    state.cart[i].quantity += action.payload.quantity;
-                    itemExists = true;
-                    break;
-                }
-            }
-
-            if (!itemExists) {
-                state.cart = [
-                    ...state.cart,
-                    {
-                        itemID: action.payload.itemID,
-                        quantity: action.payload.quantity,
-                    },
-                ];
-            }
-            immutableCalculateCartTotal(state);
-        },
-
-        // reduces the quantity of an item in a cart if quantity > 0
-        // if quantity is 1 and user hits delete, item should be removed from list
-        deleteItemFromCart: (state, action: PayloadAction<number>) => {
-            for (let i = 0; i < state.cart.length; i++) {
-                if (state.cart[i].itemID === action.payload && state.cart[i].quantity > 1) {
-                    state.cart[i].quantity -= 1;
-                    break;
-                } else if (state.cart[i].itemID === action.payload && state.cart[i].quantity === 1) {
-                    // if state.cart[i].quantity is 1, then filter out that item altogether.
-                    state.cart = state.cart.filter(
-                        (item) => item.itemID !== action.payload
-                    );
-                    break;
-                }
-            }
-            immutableCalculateCartTotal(state);
-        },
+  name: 'cart',
+  initialState,
+  reducers: {
+    toggleIsOpenFromCartSheet: (state) => {
+      state.isOpenFromCartSheet = !state.isOpenFromCartSheet;
     },
+
+    setPageViewingStoreID: (state, action: PayloadAction<number>) => {
+      state.pageViewingStoreID = action.payload;
+    },
+
+    setStoreID: (state, action: PayloadAction<number>) => {
+      state.storeID = action.payload;
+    },
+
+    // if the user adds an item from a different store, we need to reset the storeID and cart to the storeID of the new item they were looking at
+    resetCartNewStore: (state, action: PayloadAction<number>) => {
+      state.storeID = action.payload;
+      state.cart = [];
+      immutableCalculateCartTotal(state);
+    },
+
+    // the user adds an item, passing the itemID
+    addItemToCart: (state, action: PayloadAction<ICartItem>) => {
+      // loop through cart items to see if the itemID (action.payload) exists.
+      // if it exists, then just increment the quantity.
+      // if it does not exist, then add the new item to a copy of the existing cart state.
+
+      let itemExists = false;
+
+      for (let i = 0; i < state.cart.length; i++) {
+        if (state.cart[i].itemID === action.payload.itemID) {
+          state.cart[i].quantity += action.payload.quantity;
+          itemExists = true;
+          break;
+        }
+      }
+
+      if (!itemExists) {
+        state.cart = [
+          ...state.cart,
+          {
+            itemID: action.payload.itemID,
+            quantity: action.payload.quantity,
+          },
+        ];
+      }
+      immutableCalculateCartTotal(state);
+    },
+
+    // reduces the quantity of an item in a cart if quantity > 0
+    // if quantity is 1 and user hits delete, item should be removed from list
+    deleteItemFromCart: (state, action: PayloadAction<number>) => {
+      for (let i = 0; i < state.cart.length; i++) {
+        if (state.cart[i].itemID === action.payload && state.cart[i].quantity > 1) {
+          state.cart[i].quantity -= 1;
+          break;
+        } else if (state.cart[i].itemID === action.payload && state.cart[i].quantity === 1) {
+          // if state.cart[i].quantity is 1, then filter out that item altogether.
+          state.cart = state.cart.filter(
+            (item) => item.itemID !== action.payload,
+          );
+          break;
+        }
+      }
+      immutableCalculateCartTotal(state);
+    },
+  },
 });
 
-export const { toggleIsOpenFromCartSheet, setPageViewingStoreID, setStoreID, resetCartNewStore, addItemToCart, deleteItemFromCart } =
-    cartSlice.actions;
+export const {
+  toggleIsOpenFromCartSheet, setPageViewingStoreID, setStoreID, resetCartNewStore, addItemToCart, deleteItemFromCart,
+} = cartSlice.actions;
 export default cartSlice.reducer;
--- components/StoreComponents/CartOverviewComponent/CartOverview.tsx
+++ components/StoreComponents/CartOverviewComponent/CartOverview.tsx
@@ -1,32 +1,32 @@
-import Link from "next/link";
-import styled from "styled-components";
-import CheckoutButton from "../CheckoutButton/CheckoutButton";
-import CartItem from "../CartItem/CartItem";
-import CarrotRight from "../../Icons/CarrotRightIcon";
-import TableOfFood from "../../SVGgraphics/TableOfFood";
+import Link from 'next/link';
+import styled from 'styled-components';
+import CheckoutButton from '../CheckoutButton/CheckoutButton';
+import CartItem from '../CartItem/CartItem';
+import CarrotRight from '../../Icons/CarrotRightIcon';
+import TableOfFood from '../../SVGgraphics/TableOfFood';
 
-import { restaurantList } from "../../datav2";
+import { restaurantList } from '../../datav2';
 
 // redux global state
-import { useAppDispatch, useAppSelector } from "../../../app-redux/hooks";
-import { resetCartNewStore } from "../../../app-redux/features/cart/cartSlice";
+import { useAppDispatch, useAppSelector } from '../../../app-redux/hooks';
+import { resetCartNewStore } from '../../../app-redux/features/cart/cartSlice';
 
-const CartOverview__wrapper = styled.aside<{isInCartSheet: boolean}>`
+const CartOverview__wrapper = styled.aside<{ isInCartSheet: boolean }>`
     display: flex;
     flex-direction: column;
-    width: ${props => props.isInCartSheet ? `100%` : `340px`};
+    width: ${(props) => (props.isInCartSheet ? '100%' : '340px')};
     border-left: 1px solid var(--primary-gray);
-    position: ${props => props.isInCartSheet ? `relative` : `fixed`};
-    right: ${props => props.isInCartSheet ? `unset` : `0`};
-    height: ${props => props.isInCartSheet ? `100%` : `calc(100% - 64px)`};
-    box-shadow: ${props => props.isInCartSheet ? `rgb(0 0 0 / 20%) 0px 8px 24px;` : `none`};
-    /* padding-top: ${props => props.isInCartSheet ? `72px` : `0`}; */
+    position: ${(props) => (props.isInCartSheet ? 'relative' : 'fixed')};
+    right: ${(props) => (props.isInCartSheet ? 'unset' : '0')};
+    height: ${(props) => (props.isInCartSheet ? '100%' : 'calc(100% - 64px)')};
+    box-shadow: ${(props) => (props.isInCartSheet ? 'rgb(0 0 0 / 20%) 0px 8px 24px;' : 'none')};
+    /* padding-top: ${(props) => (props.isInCartSheet ? '72px' : '0')}; */
     overflow-y: scroll;
     background-color: var(--primary-white);
 
     @media screen and (max-width: 1185px) {
         // hide the component for now - it will show itself when the shopping cart button is triggered
-        display: ${props => props.isInCartSheet ? `flex` : `none`};
+        display: ${(props) => (props.isInCartSheet ? 'flex' : 'none')};
     }
 `;
 
@@ -93,69 +93,68 @@ const CartOverview__zeroItems__span = styled.span`
 `;
 
 type TCartOverview = {
-    isInCartSheet: boolean;
-    children?: JSX.Element;
-}
+  isInCartSheet: boolean;
+  children?: JSX.Element;
+};
 
 export default function CartOverview({ isInCartSheet, children }: TCartOverview) {
-    const numberofitems = useAppSelector((state) => state.cartSlice.cart[0]?.quantity);
-    
-    const restaurants = restaurantList;
+  const numberofitems = useAppSelector((state) => state.cartSlice.cart[0]?.quantity);
 
-    // consume store, get storeID
-    const storeID = useAppSelector((state) => state.cartSlice.storeID) as keyof typeof restaurants;
+  const restaurants = restaurantList;
 
-    // consume store, get cart array
-    const cart = useAppSelector((state) => state.cartSlice.cart);
-    
-    const arrayOfCartItems = cart.map((item) => {
-        return (
-            <CartItem 
-                imageSrc={restaurants[storeID].storefrontData.items[item.itemID].image.src}
-                imageAlt={restaurants[storeID].storefrontData.items[item.itemID].image.alt}
-                itemName={restaurants[storeID].storefrontData.items[item.itemID].itemName}
-                price={restaurants[storeID].storefrontData.items[item.itemID].price}
-                quantity={item.quantity}
-                itemID={item.itemID}
-                key={restaurants[storeID].storefrontData.items[item.itemID].image.src}
-            />
+  // consume store, get storeID
+  const storeID = useAppSelector((state) => state.cartSlice.storeID) as keyof typeof restaurants;
+
+  // consume store, get cart array
+  const cart = useAppSelector((state) => state.cartSlice.cart);
+
+  const arrayOfCartItems = cart.map((item) => (
+    <CartItem
+      imageSrc={restaurants[storeID].storefrontData.items[item.itemID].image.src}
+      imageAlt={restaurants[storeID].storefrontData.items[item.itemID].image.alt}
+      itemName={restaurants[storeID].storefrontData.items[item.itemID].itemName}
+      price={restaurants[storeID].storefrontData.items[item.itemID].price}
+      quantity={item.quantity}
+      itemID={item.itemID}
+      key={restaurants[storeID].storefrontData.items[item.itemID].image.src}
+    />
+  ));
+
+  return (
+    <CartOverview__wrapper isInCartSheet={isInCartSheet}>
+      {children || null}
+      {numberofitems > 0
+        ? (
+          <>
+            <CartOverview__checkout__wrapper>
+              <CartOverview__checkout__description>
+                <CartOverview__checkout__header>
+                  Your cart from
+                </CartOverview__checkout__header>
+                <CartOverview__checkout__link
+                  href={`/store/${storeID}`}
+                >
+                  {restaurants[storeID].restaurantData.restaurantName}
+                  <CarrotRight />
+                </CartOverview__checkout__link>
+              </CartOverview__checkout__description>
+              <CheckoutButton />
+            </CartOverview__checkout__wrapper>
+            <CartOverview__list__wrapper>
+              {arrayOfCartItems}
+            </CartOverview__list__wrapper>
+          </>
         )
-    })
-    
-    return (
-        <CartOverview__wrapper isInCartSheet={isInCartSheet}>
-            {!!children ? children : null}
-            {numberofitems > 0 ?
-                <>
-                    <CartOverview__checkout__wrapper>
-                        <CartOverview__checkout__description>
-                            <CartOverview__checkout__header>
-                                Your cart from
-                            </CartOverview__checkout__header>
-                            <CartOverview__checkout__link
-                                href={`/store/${storeID}`}
-                            >
-                                {restaurants[storeID].restaurantData.restaurantName}
-                                <CarrotRight />
-                            </CartOverview__checkout__link>
-                        </CartOverview__checkout__description>
-                        <CheckoutButton />
-                    </CartOverview__checkout__wrapper>
-                    <CartOverview__list__wrapper>
-                        {arrayOfCartItems}
-                    </CartOverview__list__wrapper>
-                </>
-            :
-                <CartOverview__zeroItems>
-                    <TableOfFood />
-                    <CartOverview__zeroItems__span__wrapper>
-                        <CartOverview__zeroItems__span>Your cart is empty</CartOverview__zeroItems__span>
-                        <CartOverview__zeroItems__span>Add items to get started</CartOverview__zeroItems__span>
-                    </CartOverview__zeroItems__span__wrapper>
-                </CartOverview__zeroItems>
-            }
-            
-            
-        </CartOverview__wrapper>
-    );
+        : (
+          <CartOverview__zeroItems>
+            <TableOfFood />
+            <CartOverview__zeroItems__span__wrapper>
+              <CartOverview__zeroItems__span>Your cart is empty</CartOverview__zeroItems__span>
+              <CartOverview__zeroItems__span>Add items to get started</CartOverview__zeroItems__span>
+            </CartOverview__zeroItems__span__wrapper>
+          </CartOverview__zeroItems>
+        )}
+
+    </CartOverview__wrapper>
+  );
 }
