9f1bfc3cbcbee838020acf89dc137eb683db2cdc
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-02-08 17:49:48 -0500

    set up first reducer, setRestaurantData

--- app-redux/features/data/dataSlice.tsx
+++ app-redux/features/data/dataSlice.tsx
@@ -1,5 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { TRestaurantList, TRestaurantsCarousel } from '../global.d.ts';
+
+const initialState = {
+    restaurantListData: {},
+    restaurantCarousels: {},
+};
 
 const dataSlice = createSlice({
     // TODO: add data fetched from _app.tsx to dataSlice. Use a payload tied to action
+    name: 'restaurantData',
+    initialState,
+    // todo: try to add restaurantCarousels somehow?
+    reducers: {
+        setRestaurantData: (state, action: PayloadAction<TRestaurantList>) => {
+            state.restaurantListData = action.payload;
+        }
+    }
 });
